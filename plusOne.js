0fb129254b1dce60cb4f7a80d7a7d730d6cef358
Renan Cidale Assumpcao <renancidale@hotmail.com>
2020-06-15 10:29:09 +0200

    Create plusOne.js

---
+++ plusOne.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    
+    for(let i = digits.length-1; i>=0; i--) {
+        if(digits[i] === 9) {
+            digits[i] = 0;
+            if(i === 0) {
+                digits.splice(0, 0, 1)
+            }
+        } else {
+            digits[i] += 1;
+            break;
+        }
+    }
+    
+    return digits
+    
+    
+};
